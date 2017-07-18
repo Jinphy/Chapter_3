775902fdfc3f1f749c669081e627a2a2eee8ad04
Jinphy <709584408@qq.com>
2017-07-18 16:48:32 +0800

    add alpha and translation animation

--- app/src/main/java/com/example/jinphy/chapter_3/MainActivity.java
+++ app/src/main/java/com/example/jinphy/chapter_3/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.jinphy.chapter_3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = ((Button) findViewById(R.id.button));
+        TranslateAnimation animation = (TranslateAnimation) getResources().getAnimation(R.anim.trans_anim);
+
     }
+
+
 }
